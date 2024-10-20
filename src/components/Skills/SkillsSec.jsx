d8c4e2ffd605b57c671233e1244239672e92ba7d
martin0626 <scbsteel12@gmail.com>
2024-10-20 13:08:59 +0300

    Started Skills section and Finisihed Navigation/Hero sections

--- src/components/Skills/SkillsSec.jsx
+++ src/components/Skills/SkillsSec.jsx
@@ -1,8 +1,16 @@
+import TechStack from "./SkillsTech";
+
 export default function Skills() {
 
     return (
-      <section className="skills">
-        
-      </section>
+      <>
+        <section className="skills">
+          <div className="skills-header">
+            <h1 className="mainHeader white-text">Hi, I’m Martin. Nice to meet you!</h1>
+            <h2 className="white-text">Since beginning my journey as a freelance designer 12 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops.</h2>
+          </div>
+        </section>
+        <TechStack/>
+      </>
     )
   }
---
+++ src/components/Skills/SkillsTech.jsx
@@ -0,0 +1,49 @@
+export default function TechStack(){
+    return (
+        <section className="tech-stack">
+
+            <div className="stack-description">
+                <h1 className="secondaryHeader">My Tech Stack</h1>
+            </div>
+            <div className="stack">
+            <div className="singleTech">
+                <div className="singleTech-logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JS Logo" />
+                </div>
+                <p>JavaScript</p>
+            </div>
+            <div className="singleTech">
+                <div className="singleTech-logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JS Logo" />
+                </div>
+                <p>JavaScript</p>
+            </div>
+            <div className="singleTech">
+                <div className="singleTech-logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JS Logo" />
+                </div>
+                <p>JavaScript</p>
+            </div>
+            <div className="singleTech">
+                <div className="singleTech-logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JS Logo" />
+                </div>
+                <p>JavaScript</p>
+            </div>
+            <div className="singleTech">
+                <div className="singleTech-logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JS Logo" />
+                </div>
+                <p>JavaScript</p>
+            </div>
+            <div className="singleTech">
+                <div className="singleTech-logo">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="JS Logo" />
+                </div>
+                <p>JavaScript</p>
+            </div>
+            </div>
+          
+        </section>
+    )
+}
