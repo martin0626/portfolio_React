e78ac20d05de085ce9366c26593b295cd8dec1be
martin0626 <scbsteel12@gmail.com>
2024-10-29 19:22:24 +0200

    Updated Form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+import ContactInput from "./ContactInput";
+
 export default function ContactForm(){
+    const [selectedInput, setSelectedInput] = useState('');
+
     return(
         <form className="contact-form">
             
@@ -7,33 +12,26 @@ export default function ContactForm(){
                 <p></p>
             </div>
             <h3 className="secondaryHeader">Get in touch</h3>
-            <div className="contact-form-input">
-                <label> Name:</label>
-                <input
-                    className="text-euro"
-                    type="text"
-                    name="name"
-                    required
-                />
-            </div>
-            <div className="contact-form-input">
-                <label> Email:</label>
-                <input
-                    className="text-euro"
-                    type="email"
-                    name="email"
-                    required
-                />
-            </div>
-            <div className="contact-form-input ">
-                <label> Message:</label>
-                <textarea
-                    className="text-euro"
-                    type="text"
-                    name="message"
-                    required
-                />
-            </div>
+            <ContactInput 
+                label={'Name'} 
+                className="text-euro"
+                type="text"
+                name="name"
+            />
+            <ContactInput 
+                label={'Email'} 
+                className="text-euro"
+                type="email"
+                name="email"
+            />
+            <ContactInput
+                typeElement='textarea'
+                label={'Message'} 
+                className="text-euro"
+                type="text"
+                name="message"
+                rows="4"
+            />
             <button className="submitBtn text-euro" type="submit">Send</button>
       </form>
     )
---
+++ src/components/Contact/ContactInput.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export default function ContactInput({typeElement='input', label, ...props}){
+    const [selectedInput, setSelectedInput] = useState(false);
+
+    const handleFocus = ()=>{
+        setSelectedInput(true);
+    }
+
+    const handleBlur = (e)=>{
+        if(e.target.value.trim() === ''){
+            setSelectedInput(false);
+        }
+        
+    }
+
+    return(
+        <div className="contact-form-input">
+            <label className={selectedInput && "selected-label"}>{label}</label>
+            {
+                typeElement === 'input'
+                    ?
+                <input onFocus={handleFocus} onBlur={handleBlur} {...props} required/>
+                    :
+                <textarea onFocus={handleFocus} onBlur={handleBlur} {...props} required/>
+            }
+
+        </div>
+    )
+}
