0bae8ddc4c902e892ccab4fa2c830163260159e6
martin0626 <scbsteel12@gmail.com>
2024-11-17 13:13:19 +0200

    Added Gallery In Resume Section

--- src/components/Resume/ResumeMain.jsx
+++ src/components/Resume/ResumeMain.jsx
@@ -2,6 +2,7 @@ import resumeImg from "../../../public/ResumeImg.png"
 import resumePdf from "../../../public/MartinIvanov_Resume.pdf"
 import { useState } from "react";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+import { AnimatePresence, motion } from "motion/react";
 
 
 
@@ -9,22 +10,42 @@ const DOCS = [
     {
         title: 'Resume',
         position: 1,
-        image: '../../../public/ResumeImg.png',
+        image: '/public/ResumeImg.png',
     },
     {
-        title: 'JS Advanced Course',
+        title: 'React Certificate',
         position: 2,
-        image: '../../../public/ResumeImg.png',
+        image: '/public/React_Certificate.png',
     },
     {
-        title: 'Python Web Course',
+        title: 'HTML&CSS Certificate',
         position: 3,
-        image: '../../../public/ResumeImg.png',
+        image: '/public/HTMLCSS_Certificate.png',
     },
     {
-        title: 'React Course',
+        title: 'JS Advanced',
         position: 4,
-        image: '../../../public/ResumeImg.png',
+        image: '/public/JSAdvanced.png',
+    },
+    {
+        title: 'JS Applications',
+        position: 5,
+        image: '/public/JSapps.png',
+    },
+    {
+        title: 'Python Advanced',
+        position: 6,
+        image: '/public/PyAdvanced.png',
+    },
+    {
+        title: 'Python OOP',
+        position: 7,
+        image: '/public/PythonOOP.png',
+    },
+    {
+        title: 'Python Web Framework',
+        position: 8,
+        image: '/public/PythonWeb.png',
     }
 ]
 
@@ -32,19 +53,19 @@ const DOCS = [
 
 export default function ResumeMain(){
     // const mainDoc = DOCS.find(e=> e.position === 1);
-    const [ currentActivePos,  setCurrentActivePos] = useState(3);
+    const [ currentActivePos,  setCurrentActivePos] = useState(0);
     const [currentEffectSide, setCurrentEffectSide] = useState('');
 
-    // const maxIndex = images.length - 1;
+    const maxIndex = DOCS.length - 1;
 
 
     const handleChangeIndex = (nextIndex)=>{
-        setCurrentEffectSide(nextIndex > currentActiveIndex ? 'show-right' : 'show-left')
+        setCurrentEffectSide(nextIndex > currentActivePos ? 'show-right' : 'show-left')
         setCurrentActivePos(nextIndex);
     }
 
     const handleNextIndex = ()=>{
-        let newIndex = currentActiveIndex + 1;
+        let newIndex = currentActivePos + 1;
 
         if(maxIndex < newIndex){
             newIndex = 0;
@@ -54,7 +75,7 @@ export default function ResumeMain(){
     }
 
     const handlePreviousIndex = ()=>{
-        let newIndex = currentActiveIndex - 1;
+        let newIndex = currentActivePos - 1;
 
         if(0 > newIndex){
             newIndex = maxIndex;
@@ -64,13 +85,25 @@ export default function ResumeMain(){
         setCurrentActivePos(newIndex);
     }
 
+    const handleDragEnd = (event, info) => {
+        const offset = info.offset.x;
+        const velocity = info.velocity.x;
+    
+        // Determine direction of swipe based on drag offset and velocity
+        if (offset < -200 || velocity < -300) {
+            handleNextIndex();
+        } else if (offset > 100 || velocity > 300) {
+            handlePreviousIndex();
+        }
+      };
+
+
     const handleDownload = () => {
         const link = document.createElement('a');
         link.href = resumePdf;
         link.download = 'MartinIvanov_Resume.pdf'; 
         link.click();
     };
-
 
     return(
         <section className="resume">
@@ -81,26 +114,79 @@ export default function ResumeMain(){
                 </div>
                 
             </div>
-            <p className="resume-label">{DOCS.find(d=>d.position === currentActivePos).title}</p>
+            <p className="resume-label">{DOCS[currentActivePos].title}</p>
 
             
             <div className="resume-main">
                 <div className="resume-main-arrow leftA">
-                    <FaArrowAltCircleLeft />
+                    <FaArrowAltCircleLeft onClick={handlePreviousIndex} />
                 </div>
+
+                <AnimatePresence>
+                
+                    <motion.div 
+                        className="resume-main-img"
+                        
+                    >
+                        {
+                            DOCS.map((doc, index)=>{
+                                if(currentActivePos === index){
+                                    return <motion.img 
+                                    drag="x"
+                                    onDragEnd={handleDragEnd}
+                                    dragConstraints={{ left: 0, right: 0 }}
+                                    initial={{ opacity: 0, x: currentEffectSide ===  'show-right' ? 300 : -300}}
+                                    animate={{ opacity: 1, x: 0 }}
+                                    transition={{duration: 0.5}}
+                                    layout exit={{ x: 300, opacity: 0 }} 
+                                    key={doc.position}  
+                                    src={doc.image}
+                                    />
+                                }
+                            })
+                        }
+                    </motion.div>
+                    <div className="gallery-dots">
+                        {DOCS.map((e, i)=>{
+                            return <div className={i == currentActivePos ? 'gallery-dots-single active-dot-r' : 'gallery-dots-single'} onClick={()=>{handleChangeIndex(i)}}></div>
+                        })}
+                    </div>
+                </AnimatePresence>
                 <div className="resume-main-arrow rightA">
-                    <FaArrowAltCircleRight />
-                </div>
-                <div className="resume-main-img">
-                    {
-                        DOCS.map(doc=>{
-                            if(currentActivePos === doc.position){
-                                return <img src={doc.image}/>
-                            }
-                        })
-                    }
+                    <FaArrowAltCircleRight onClick={handleNextIndex} />
                 </div>
             </div>
         </section>
     )
 }
+
+
+
+
+
+
+
+            // <motion.div
+            //         drag="x"
+            //         dragConstraints={{ left: 0, right: 0 }}
+            //         onDragEnd={handleDragEnd}
+            //         initial={{ x: -100 }}
+            //         animate={{ x: 0 }}
+            //         transition={{ type: "spring", stiffness: 300, damping: 30 }}
+            //         style={{
+            //             display: "flex",
+            //             width: "max-content",
+            //             cursor: "grab",
+            //         }}
+            //         className="image-gallery resume-main-img"
+            //     >
+            //         {DOCS.map((obj, index) => {
+            //             if(index === currentActivePos){
+            //                 return <motion.img
+            //                 key={index}
+            //                 src={obj.image}
+            //                 alt={`Slide ${index + 1}`}
+            //             />
+            //             }
+            //     })}
+            //     </motion.div>
