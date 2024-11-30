2eff2396ea98c806e93ba91a2a278389a9ced4cd
martin0626 <scbsteel12@gmail.com>
2024-11-30 16:05:54 +0200

    Added Small Changes

--- src/components/PersonalProects/Projects.jsx
+++ src/components/PersonalProects/Projects.jsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import SingleProject from "./SingleProject";
 import projectsObj from "../../data/projects";
+import { FaGithub } from "react-icons/fa";
 
 
 export default function Projects(){
@@ -8,11 +9,19 @@ export default function Projects(){
     const projects = useMemo(()=>projectsObj.getProjects(), []);
 
 
+    const onVistHandler = (link)=>{
+        window.open(link);
+    }
+
     return(
         <section className="projects">
             <div className="projects-header">
                 <h1 className="mainHeader">My Personal Projects</h1>
-                <p>Since beginning my journey as a freelance designer 12 years ago, I've done remote work for agencies.</p>
+                <p>Here you can see part of my projects, which I created with learning purposes. Check all my projects and exercises I have moved through, on my GitHub profile below.</p>
+            </div>
+            <div className="projects-git">
+                <FaGithub onClick={()=>onVistHandler("https://github.com/martin0626")}/>
+                <p>GitHub</p>
             </div>
             <div className="projects-content">
                 {
